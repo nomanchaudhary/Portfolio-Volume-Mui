9121cc210b442fd14318e61074dfe6c2617d1cdd
nomanchaudhary <nomanchaudhary1618@gmail.com>
2025-09-22 11:01:37 +0500

    New Chnages Added

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -1,34 +1,39 @@
 export const achievementData = {
-    bio : "Every project I complete is a step forward in my journey of growth. From mastering the basics of HTML, CSS, and JavaScript to building dynamic React applications and exploring MERN and AI/ML, I see each milestone as proof of persistence and passion for technology. My achievements remind me that learning never stops and every small win builds the foundation for bigger success.",
-    achievements : [
-        {
-            id : 1,
-            title : 'Udemy React JS Certification',
-            details : 'Udemy’s React.js certification validates my front-end development skills and highlights my ability to build modern, interactive, and scalable web applications using React.',
-            date : 'Jan 20, 2025',
-            field : 'Development',
-            image : 'https://images.unsplash.com/photo-1633356122544-f134324a6cee?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-        },
-        {
-            id : 2,
-            title : 'Front-End Web Developer Certification',
-            details : 'Building responsive and visually appealing websites using HTML, CSS, Bootstrap, and JavaScript.',
-            date : 'Aug 20, 2022',
-            field : 'Development',
-            image : 'https://images.unsplash.com/photo-1593720213428-28a5b9e94613?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D0'
-        },
-        {
-            id : 3,
-            title : 'Professional Certificate in Machine Learning and Artificial Intelligence',
-            details : 'Developing intelligent solutions using Artificial Intelligence and Machine Learning to analyze data, recognize patterns, and automate decision-making.',
-            date : 'Aug 20, 2019',
-            field : 'Ai/ML',
-            image : 'https://images.unsplash.com/photo-1697577418970-95d99b5a55cf?q=80&w=996&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-        }
-
-    ]
-}
-
+  bio: "Every project I complete is a step forward in my journey of growth. From mastering the basics of HTML, CSS, and JavaScript to building dynamic React applications and exploring MERN and AI/ML, I see each milestone as proof of persistence and passion for technology. My achievements remind me that learning never stops and every small win builds the foundation for bigger success.",
+  achievements: [
+    {
+      id: 1,
+      title: "Udemy React JS Certification",
+      details:
+        "Udemy’s React.js certification validates my front-end development skills and highlights my ability to build modern, interactive, and scalable web applications using React.",
+      date: "Jan 20, 2025",
+      field: "Development",
+      image:
+        "https://images.unsplash.com/photo-1633356122544-f134324a6cee?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      id: 2,
+      title:
+        "Professional Certificate in Machine Learning and Artificial Intelligence",
+      details:
+        "Developing intelligent solutions using Artificial Intelligence and Machine Learning to analyze data, recognize patterns, and automate decision-making.",
+      date: "Jun 10, 2025",
+      field: "AI/ML",
+      image:
+        "https://images.unsplash.com/photo-1697577418970-95d99b5a55cf?q=80&w=996&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      id: 3,
+      title: "Front-End Web Developer Certification",
+      details:
+        "Building responsive and visually appealing websites using HTML, CSS, Bootstrap, and JavaScript.",
+      date: "Aug 20, 2022",
+      field: "Development",
+      image:
+        "https://images.unsplash.com/photo-1593720213428-28a5b9e94613?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D0",
+    },
+  ],
+};
 
 // Do not remove any fields.
 // Leave it blank instead as shown below.
